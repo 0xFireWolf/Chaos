bb5bb29f92cbebddbdfcf81c90e69d03c1351df1
FireWolf <austere.j@gmail.com>
2022-05-15 20:24:25 -0700

    Utilities: All installation helpers now check the return code of the package manager.

--- Utilities.py
+++ Utilities.py
@@ -6,21 +6,46 @@ import subprocess
 
 
 def brew_install(packages: list[str]) -> None:
-    subprocess.run(["brew", "install"] + packages)
+    """
+    Use Homebrew to install the given list of packages
+    :param packages: Name of the packages
+    :raise `CalledProcessError` on error.
+    """
+    subprocess.run(["brew", "install"] + packages).check_returncode()
 
 
 def apt_install(packages: list[str]) -> None:
-    subprocess.run(["sudo", "apt", "-y", "install"] + packages)
+    """
+    Use APT to install the given list of packages
+    :param packages: Name of the packages
+    :raise `CalledProcessError` on error.
+    """
+    subprocess.run(["sudo", "apt", "-y", "install"] + packages).check_returncode()
 
 
 def apt_add_repository(name: str) -> None:
-    subprocess.run(["sudo", "add-apt-repository", "-y", name])
+    """
+    Add an APT repository of the given name
+    :param name: Name of the repository
+    :raise `CalledProcessError` on error.
+    """
+    subprocess.run(["sudo", "add-apt-repository", "-y", name]).check_returncode()
 
 
 def pip_install(packages: list[str]) -> None:
-    subprocess.run(["sudo", "pip", "install"] + packages)
+    """
+    Use Pip to install the given list of Python package
+    :param packages: Name of the packages
+    :raise `CalledProcessError` on error.
+    """
+    subprocess.run(["sudo", "pip", "install"] + packages).check_returncode()
 
 
 def winget_install(packages: list[str]) -> None:
+    """
+    Use winget to install the given list of packages
+    :param packages:  Name of the packages
+    :raise `CalledProcessError` on error.
+    """
     for package in packages:
-        subprocess.run(["winget", "install", package])
+        subprocess.run(["winget", "install", package]).check_returncode()
