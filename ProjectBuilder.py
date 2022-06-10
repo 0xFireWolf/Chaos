f1c8bf6df34eb12f0ec29abf8108197c23c12dd3
FireWolf <austere.j@gmail.com>
2022-06-10 09:06:56 -0700

    ProjectBuilder: Adjust the build type accordingly on Windows. Reference: https://stackoverflow.com/questions/19024259/how-to-change-the-build-type-to-release-mode-in-cmake, https://github.com/conan-io/conan/issues/2147

--- ProjectBuilder.py
+++ ProjectBuilder.py
@@ -26,7 +26,7 @@ class ProjectBuilder:
         subprocess.run(["conan", "install", "..", "--build", "missing",
                         "--profile", "../" + profile], cwd=kBuildFolder)
         subprocess.run(["cmake", "-S", ".", "-B", kBuildFolder, "-DCMAKE_BUILD_TYPE={}".format(btype)])
-        subprocess.run(["cmake", "--build", kBuildFolder, "--clean-first", "--parallel", str(os.cpu_count())])
+        subprocess.run(["cmake", "--build", kBuildFolder, "--config", btype.value, "--clean-first", "--parallel", str(os.cpu_count())])
 
     def rebuild_project_debug(self) -> None:
         """
