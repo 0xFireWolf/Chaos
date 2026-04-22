8dd0a7f8e4bcc6f6321e35c857f77aa1aebca3eb
FireWolf <austere.j@gmail.com>
2026-04-22 16:01:56 -0700

    ProjectBuilder: Remove the deprecated `cmake_install()` which has been replaced by `CMake.install()`.

--- ProjectBuilder.py
+++ ProjectBuilder.py
@@ -144,18 +144,6 @@ class ProjectBuilder:
         # Build the project
         cmake.call(args)
 
-    def cmake_install(self, cmake: CMake, prefix: Path = None) -> None:
-        """
-        [Action] [Step] Use CMake to install the project artifacts
-        :param cmake: The CMake binary that will be used to install all artifacts
-        :param prefix: The installation prefix
-        """
-        print(f"Installing project artifacts using CMake v{cmake.major}.{cmake.minor}.{cmake.patch}...")
-        args = ["--install", self.project.build_directory]
-        if prefix is not None:
-            args.extend(["--prefix", prefix])
-        cmake.call(args)
-
     #
     # MARK: - Rebuild Project
     #
